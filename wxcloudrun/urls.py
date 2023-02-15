71f810216582df8ff837cf625d583bc044aed96c
loneve <2050396327@qq.com>
2023-02-15 16:08:04 +0800

    访问openAI

--- wxcloudrun/urls.py
+++ wxcloudrun/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls import url
 urlpatterns = (
     # 计数器接口
     url(r'^^api/count(/)?$', views.counter),
-    url('^^api/ai(/)?$', views.ai),
+    url('ai/', views.ai),
     # 获取主页
     url(r'(/)?$', views.index),
 )
